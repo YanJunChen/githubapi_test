f8f75557054a8eb65db738499f9b64d44135de87
YanJunChen <v-yanjunchen@microsoft.com>
2024-08-13 14:30:47 +0800

    init action code.

---
+++ main.py
@@ -0,0 +1,19 @@
+import requests
+import os
+
+github_headers = os.environ["GH_HDR"]
+
+def invite_collaborator(username):
+    url = f'https://api.github.com/repos/microsoft/EPSO-Server-Program-Community/collaborators/{username}'
+
+    response = requests.put(url, headers=github_headers)
+    print("response.status_code = " , response.status_code)
+
+    if response.status_code == 201:
+        print(f'Successfully invited.')
+    elif response.status_code == 204:
+        print(f'{username} is a collaborator.')
+    else:
+        print(f'Failed to invite {username}. Response: {response.status_code} - {response.json()}')
+
+invite_collaborator("eikchen0713")
