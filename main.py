cf58c772e51e535c6fe1ef0ea27bb3874d3b10a8
YanJunChen <v-yanjunchen@microsoft.com>
2024-08-14 15:02:54 +0800

    Hide api url

--- main.py
+++ main.py
@@ -8,18 +8,10 @@ github_headers = {
 }
 
 def invite_collaborator(username):
-    url = f'https://api.github.com/repos/YanJunChen/githubapi_test/collaborators/{username}'
-
+    #url = f'https://api.github.com/repos/YanJunChen/githubapi_test/collaborators/{username}'
+    url = os.environ["GH_INV_API"]+{username}
     response = requests.put(url, headers=github_headers)
     print("response.status_code = " , response.status_code)
 
-    if response.status_code == 201:
-        print(f'Successfully invited.')
-    elif response.status_code == 204:
-        print(f'{username} is a collaborator.')
-    else:
-        print(f'Failed to invite {username}. Response: {response.status_code} - {response.json()}')
-
 if __name__ == "__main__":
-    print(github_headers)
     invite_collaborator("eikchen0713")
