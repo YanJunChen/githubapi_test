ae8abf890eac28ee80be416a2b00e03f1b0558e7
YanJunChen <v-yanjunchen@microsoft.com>
2024-08-16 15:50:52 +0800

    update python command

--- main.py
+++ main.py
@@ -3,12 +3,11 @@ import os
 
 github_headers = {
     "Authorization": os.environ["GH_HDR"] ,
-}
+};
 
 def invite_collaborator(username):
-    url = os.environ["GH_INV_API"] + username
-    response = requests.put(url, headers=github_headers)
-    print("response.status_code = " , response.status_code)
+    url = os.environ["GH_INV_API"] + username;
+    response = requests.put(url, headers=github_headers);
+    print(response.status_code);
 
-if __name__ == "__main__":
-    invite_collaborator("eikchen0713")
+invite_collaborator("eikchen0713");
